1065ccda2b42a48da2aeee0dd1799b1259e78e0f
RyanMachadoo <94652639+RyanMachadoo@users.noreply.github.com>
2022-03-11 00:33:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask('app')
+
+@app.route('/')
+def principal():
+  return '<h1>Olá Mundo!</h1>'
+@app.route('/unifran')
+def unifran():
+  return '<h2>Universidade de Franca</h2>'
+@app.route('/dashboard/<nome>/')
+def dash(nome):
+  return f'Olá, {nome}'
+app.run(host='0.0.0.0', port=8080)
